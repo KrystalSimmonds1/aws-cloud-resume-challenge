06a4422a8d4fd704d2aca3aa2bc3c5e135433251
KrystalSimmonds1 <krystalsimmonds61@gmail.com>
2024-12-01 12:39:53 +0000

    Added terraform main configuration file

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.77.0"
+    }
+  }
+
+  required_version = ">= 1.9.7"
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
