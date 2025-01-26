c0d249de14f2097b830b04579d737d48a8d89afc
KrystalSimmonds1 <krystalsimmonds61@gmail.com>
2025-01-26 16:08:06 +0000

    Added bucket ownership rules and access rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,3 +21,29 @@ resource "aws_s3_bucket" "resume_bucket" {
     Name = var.bucket_name
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "resume_bucket" {
+  bucket = aws_s3_bucket.resume_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "resume_bucket" {
+  depends_on = [aws_s3_bucket_ownership_controls.resume_bucket]
+
+  bucket = aws_s3_bucket.resume_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_website_configuration" "resume_site" {
+  bucket = aws_s3_bucket.resume_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = error.html
+  }
+}
